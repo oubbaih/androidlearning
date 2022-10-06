f1b6cc2e5804ce3d403849c5f3026fd1e27c684d
oubbaih <eoubbaih@gmail.com>
2022-10-06 17:47:27 +0100

    ss:

---
+++ app/src/main/java/com/example/testtt/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.testtt
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val inp = findViewById<EditText>(R.id.editTextTextPersonName3)
+        val tt = findViewById<TextView>(R.id.textView2)
+        val btnn = findViewById<Button>(R.id.button)
+
+        btnn.setOnClickListener {
+            var inputvalue = inp.text.toString().toInt()
+
+            when(inputvalue){
+                in 2..5 -> tt.text = "hello $inputvalue"
+                in 6..12 -> tt.text = "hi $inputvalue"
+                in 12 downTo 1 -> tt.text = "down $inputvalue"
+                else -> tt.text = "unvalide Number"
+            }
+
+        }
+
+
+    }
+}
+
